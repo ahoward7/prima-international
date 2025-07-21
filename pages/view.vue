09e4480fcf2944a9806cec8f60b353a6a58b458c
Avery Howard <avery.d.howard@gmail.com>
2025-07-21 16:08:45 -0400

    Bold and semibold text for machine view

--- pages/view.vue
+++ pages/view.vue
@@ -1,68 +1,68 @@
 <template>
   <div class="h-screen flex justify-center">
-    <div class="w-full flex flex-col gap-8 p-8 max-w-[1200px]">
-      <HeaderPrimary>{{ machine.serialNumber}}</HeaderPrimary>
+    <div class="w-full flex flex-col gap-8 p-8 mt-4 max-w-[1200px]">
+      <HeaderPrimary class="mb-4">{{ machine.serialNumber || `${machine.model} (NO SERIAL#)` }}</HeaderPrimary>
       <div class="grid grid-cols-2 gap-16">
         <div class="flex flex-col gap-2 bg-gray-200 px-4 py-3 border border-prima-red">
           <HeaderSecondary class="mb-2">Machine Information</HeaderSecondary>
-          <div class="flex justify-between font-bold">
-            <label class="w-32">Model</label>
-            <span class="text-prima-red">{{ machine.model }}</span>
+          <div class="flex justify-between">
+            <label class="w-32 font-bold">Model</label>
+            <span class="text-prima-red font-semibold">{{ machine.model }}</span>
           </div>
-          <div class="flex justify-between font-bold">
-            <label class="w-32">Price</label>
-            <span class="text-prima-red">${{ formatPrice(machine.price) }}</span>
+          <div class="flex justify-between">
+            <label class="w-32 font-bold">Price</label>
+            <span class="text-prima-red font-semibold">${{ formatPrice(machine.price) }}</span>
           </div>
-          <div class="flex justify-between font-bold">
-            <label class="w-32">Type</label>
-            <span class="text-prima-red">{{ machine.type }}</span>
+          <div class="flex justify-between">
+            <label class="w-32 font-bold">Type</label>
+            <span class="text-prima-red font-semibold">{{ machine.type }}</span>
           </div>
-          <div class="flex justify-between font-bold">
-            <label class="w-32">Year</label>
-            <span class="text-prima-red">{{ machine.year }}</span>
+          <div class="flex justify-between">
+            <label class="w-32 font-bold">Year</label>
+            <span class="text-prima-red font-semibold">{{ machine.year }}</span>
           </div>
-          <div class="flex justify-between font-bold">
-            <label class="w-32">Hours</label>
-            <span class="text-prima-red">{{ machine.hours }}</span>
+          <div class="flex justify-between">
+            <label class="w-32 font-bold">Hours</label>
+            <span class="text-prima-red font-semibold">{{ machine.hours }}</span>
           </div>
-          <div class="flex justify-between font-bold">
-            <label class="w-32">Location</label>
-            <span class="text-prima-red">{{ machine.location }}</span>
+          <div class="flex justify-between">
+            <label class="w-32 font-bold">Location</label>
+            <span class="text-prima-red font-semibold">{{ machine.location }}</span>
           </div>
         </div>
         <div class="flex flex-col gap-2 bg-prima-red/10 px-4 py-3 border border-prima-red">
           <HeaderSecondary class="mb-2">User Information</HeaderSecondary>
-          <div class="flex justify-between font-bold">
-            <label class="w-40">Contact Company</label>
-            <span class="text-prima-red">{{ machine.contact?.company }}</span>
+          <div class="flex justify-between">
+            <label class="w-40 font-bold">Contact Company</label>
+            <span class="text-prima-red font-semibold">{{ machine.contact?.company }}</span>
           </div>
-          <div class="flex justify-between font-bold">
-            <label class="w-40">Contact Name</label>
-            <span class="text-prima-red">{{ machine.contact?.name}}</span>
+          <div class="flex justify-between">
+            <label class="w-40 font-bold">Contact Name</label>
+            <span class="text-prima-red font-semibold">{{ machine.contact?.name}}</span>
           </div>
-          <div class="flex justify-between font-bold">
-            <label class="w-40">Date Created</label>
-            <span class="text-prima-red">{{ formatDate(machine.createDate) }}</span>
+          <div class="flex justify-between">
+            <label class="w-40 font-bold">Date Created</label>
+            <span class="text-prima-red font-semibold">{{ formatDate(machine.createDate) }}</span>
           </div>
-          <div class="flex justify-between font-bold">
-            <label class="w-40">Date Last Modified</label>
-            <span class="text-prima-red">{{ formatDate(machine.lastModDate) }}</span>
+          <div class="flex justify-between">
+            <label class="w-40 font-bold">Date Last Modified</label>
+            <span class="text-prima-red font-semibold">{{ formatDate(machine.lastModDate) }}</span>
           </div>
-          <div class="flex justify-between font-bold">
-            <label class="w-40">User Last Modified</label>
-            <span class="text-prima-red">{{ machine.salesman }}</span>
+          <div class="flex justify-between">
+            <label class="w-40 font-bold">User Last Modified</label>
+            <span class="text-prima-red font-semibold">{{ machine.salesman }}</span>
           </div>
         </div>
       </div>
       <DividerLine />
-      <div class="flex flex-col gap-8 font-bold">
+      <div class="flex flex-col gap-8 font-semibold">
         <div class="flex flex-col gap-1">
           <HeaderSecondary>Description</HeaderSecondary>
-          <span>{{ machine.description }}</span>
+          <span>{{ machine.description || 'No description' }}</span>
         </div>
         <div class="flex flex-col gap-1">
           <HeaderSecondary>Notes</HeaderSecondary>
-          <span>{{ machine.notes }}</span>
+          <span>{{ machine.notes || 'No notes' }}</span>
         </div>
       </div>
     </div>
@@ -73,6 +73,10 @@
 const { machine } = useMachineStore()
 
 function formatPrice(price: number = 0): string {
+  if (!price) {
+    return ''
+  }
+
   return price.toLocaleString('en-US', {
     minimumFractionDigits: 0,
     maximumFractionDigits: 0

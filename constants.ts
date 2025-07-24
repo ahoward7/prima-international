bc3e062721800ce8a761c30ab090b1d68e923fbf
Avery Howard <avery.d.howard@gmail.com>
2025-07-24 12:42:10 -0400

    Move filter options to constants file

---
+++ constants.ts
@@ -0,0 +1,37 @@
+export const filterOptions = {
+  location: [
+    { label: 'Choose location', data: '' },
+    { label: 'Located', data: 'located' },
+    { label: 'Sold', data: 'sold' },
+    { label: 'Archived', data: 'archived' },
+  ],
+  model: [
+    { label: 'Choose model', data: '' },
+    { label: '988K', data: '988K' },
+    { label: '775E', data: '775E' },
+    { label: 'A40D', data: 'A40D' },
+    { label: 'D9T', data: 'D9T' },
+    { label: '950GC', data: '950GC' },
+    { label: '772G', data: '772G' },
+    { label: 'WA9003EO', data: 'WA9003EO' },
+    { label: '950M', data: '950M' },
+    { label: '775F', data: '775F' },
+    { label: '924K', data: '924K' },
+    { label: '777G', data: '777G' },
+    { label: '544L', data: '544L' },
+    { label: '785C', data: '785C' },
+    { label: '773G', data: '773G' },
+    { label: '772', data: '772' },
+    { label: '336FL', data: '336FL' },
+    { label: '14M', data: '14M' }
+  ],
+  type: [
+    { label: 'Choose type', data: '' },
+    { label: 'WL', data: 'WL' },
+    { label: 'OHT', data: 'OHT' },
+    { label: 'ADT', data: 'ADT' },
+    { label: 'TTT', data: 'TTT' },
+    { label: 'EX', data: 'EX' },
+    { label: 'MG', data: 'MG' }
+  ]
+}
--- pages/inventory.vue
+++ pages/inventory.vue
@@ -27,6 +27,7 @@
 
 <script setup lang="ts">
 import { useDebounceFn } from '@vueuse/core'
+import { filterOptions } from '~/constants'
 
 const filters = ref<MachineFilters>({
   location: '',
@@ -36,44 +37,6 @@ const filters = ref<MachineFilters>({
   model: '',
   type: '',
 })
-
-const filterOptions = {
-  location: [
-    { label: 'Choose location', data: '' },
-    { label: 'Located', data: 'located' },
-    { label: 'Sold', data: 'sold' },
-    { label: 'Archived', data: 'archived' },
-  ],
-  model: [
-    { label: 'Choose model', data: '' },
-    { label: '988K', data: '988K' },
-    { label: '775E', data: '775E' },
-    { label: 'A40D', data: 'A40D' },
-    { label: 'D9T', data: 'D9T' },
-    { label: '950GC', data: '950GC' },
-    { label: '772G', data: '772G' },
-    { label: 'WA9003EO', data: 'WA9003EO' },
-    { label: '950M', data: '950M' },
-    { label: '775F', data: '775F' },
-    { label: '924K', data: '924K' },
-    { label: '777G', data: '777G' },
-    { label: '544L', data: '544L' },
-    { label: '785C', data: '785C' },
-    { label: '773G', data: '773G' },
-    { label: '772', data: '772' },
-    { label: '336FL', data: '336FL' },
-    { label: '14M', data: '14M' }
-  ],
-  type: [
-    { label: 'Choose type', data: '' },
-    { label: 'WL', data: 'WL' },
-    { label: 'OHT', data: 'OHT' },
-    { label: 'ADT', data: 'ADT' },
-    { label: 'TTT', data: 'TTT' },
-    { label: 'EX', data: 'EX' },
-    { label: 'MG', data: 'MG' }
-  ]
-}
 
 const searchInput = ref('')
 const displayFormat = ref('oneLine')
@@ -92,6 +55,5 @@ const { data: machines, pending } = await useFetch<Machine[]>('/machine', {
   watch: [filters]
 })
 
-const machineCategories: string[] = ['located', 'sold', 'archived', 'all']
 const displayFormats: string[] = ['oneLine', "twoLine", "twoLineTruncated"]
 </script>

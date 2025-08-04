2c8f583d3d6a4701fd016389d5d0bf66f7dd2b50
Avery Howard <avery.d.howard@gmail.com>
2025-08-03 20:10:01 -0400

    Split contact generation and update logic

--- server/routes/machine/index.post.ts
+++ server/routes/machine/index.post.ts
@@ -8,40 +8,7 @@ export default defineEventHandler(async (event: H3Event): Promise<any> => {
     return { error: 'Missing required machine or contact information' }
   }
 
-  let contact: Contact
-  let contactChanged = false
-  let contactId: string
-
-  if (machine.contact.c_id === 'new') {
-    contact = generateContact(machine.contact.name, machine.contact.company, date)
-    contactId = contact.c_id
-    await ContactSchema.create(contact)
-    contactChanged = true
-  }
-  else {
-    contact = machine.contact
-    contactId = contact.c_id
-
-    const existingContact: Contact | null = await ContactSchema.findOne({ c_id: contactId }).lean()
-
-    if (existingContact) {
-      for (const key in contact) {
-        const contactKey = key as keyof Contact
-        if (key !== 'lastModDate' && contact[contactKey] !== existingContact[contactKey]) {
-          contactChanged = true
-          break
-        }
-      }
-    }
-    else {
-      contactChanged = true
-    }
-
-    if (contactChanged) {
-      contact.lastModDate = date
-      await ContactSchema.updateOne({ c_id: contactId }, { $set: contact }, { upsert: true })
-    }
-  }
+  const { contactId, contactChanged } = await handleContactUpdateOrCreate(machine.contact, date)
 
   machine.m_id = generateRandom10DigitNumber()
   machine.contactId = contactId

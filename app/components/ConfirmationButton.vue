e75b283700361be09cef30a2878229835f6a1499
Avery Howard <avery.d.howard@gmail.com>
2025-08-04 14:20:47 -0400

    Make sure confirmation buttons are centered

--- app/components/ConfirmationButton.vue
+++ app/components/ConfirmationButton.vue
@@ -1,7 +1,7 @@
 <template>
-  <Button class="relative" tabindex="1" @focus="showConfirm = true" @focusout="showConfirm = false">
+  <Button class="relative" tabindex="1" @focus="showConfirm = true" @focusout="showConfirm = false" @click.capture="showConfirm = true">
     <slot />
-    <div v-if="showConfirm" class="absolute top-12 flex justify-center gap-1">
+    <div v-if="showConfirm" class="w-full absolute left-0 top-12 flex justify-center gap-1">
       <Icon name="carbon:close" class="text-red-600" size="28" @click="showConfirm = false" />
       <Icon name="carbon:checkmark" class="text-green-600" size="28" @click="emit('confirm')" />
     </div>

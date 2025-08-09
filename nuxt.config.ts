71925be767ef54398c163026e2e77ca4c021913b
Avery Howard <avery.d.howard@gmail.com>
2025-08-09 16:34:26 -0400

    Remove nitro settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  nitro: {
-    preset: 'static',
-    experimental: {
-      wasm: true
-    }
-  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',

c4ebaf2631f4fb9f59d0590f3dfafdcc6cdaaa11
Avery Howard <avery.d.howard@gmail.com>
2025-08-19 09:02:56 -0400

    Hover effect for button

--- app/components/button/index.vue
+++ app/components/button/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="min-w-24 h-12 text-white font-semibold bg-prima-red dark:bg-prima-dark-accent px-4 cursor-pointer">
+  <button class="min-w-24 h-12 text-white font-semibold bg-prima-red dark:bg-prima-dark-accent px-4 cursor-pointer hover:scale-105 hover:brightness-105">
     <slot />
   </button>
 </template>

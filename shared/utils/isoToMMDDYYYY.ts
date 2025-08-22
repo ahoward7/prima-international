a09d155804227e2bd7e361de611877ce10c1e629
Avery Howard <avery.d.howard@gmail.com>
2025-08-22 13:25:25 -0400

    Make sure date is formatted correctly

--- shared/utils/isoToMMDDYYYY.ts
+++ shared/utils/isoToMMDDYYYY.ts
@@ -5,7 +5,7 @@ export function isoToMMDDYYYY(isoString: string = ''): string {
 
   const date = new Date(isoString)
   const month = (date.getMonth() + 1).toString().padStart(2, '0')
-  const day = date.getDate().toString().padStart(2, '0')
+  const day = (date.getDate() + 1).toString().padStart(2, '0')
   const year = date.getFullYear()
   return `${month}/${day}/${year}`
 }
